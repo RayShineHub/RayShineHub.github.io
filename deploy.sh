8ad81657af678d00f7d80c4b0fb2ff318ccb113b
SPF <spf1773@gmail.com>
2021-12-15 23:40:43 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,9 @@ git push
 # 发布到github的prod分支，触发工作流同步到Linux服务器
 git checkout prod
 git pull
-read -p "请输入本次发版内容：" msg
-git merge -m $msg origin main
+#read -p "请输入本次发版内容：" msg
+#git merge -m $msg origin main
+git merge -m 'deploy' origin main
 git push
 git checkout main
 git pull
