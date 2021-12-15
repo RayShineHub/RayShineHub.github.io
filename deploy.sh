eb64ca076d676776e2f4e85d171e1fc117dcd20a
SPF <spf1773@gmail.com>
2021-12-15 23:02:21 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -e
 
 #	检验build是否成功
 yarn build
-rm -rf ./dist   # ./dist是build之后文件夹的位置，即config.js中配置的dest，详见vuepress官网
+#rm -rf ./dist   # ./dist是build之后文件夹的位置，即config.js中配置的dest，详见vuepress官网
 
 # 提交到main分支
 git checkout main
